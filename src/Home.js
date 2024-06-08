db097e1beb38be43f9034ec3cf37245806a89a06
unknown <pengsmemberships@gmail.com>
2024-06-08 14:15:34 -0500

    initial

---
+++ src/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://m.media-amazon.com/images/I/71CkwcSsmjL._SX3000_.jpg"
+          alt=""
+        />
+
+        <div className="home__row">
+            {/*product */}
+            {/*product */}
+        </div>
+
+        <div className="home__row">
+            {/*product */}
+            {/*product */}
+            {/*product */}
+        </div>
+
+        <div className="home__row">
+            {/*product */}
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Home;
